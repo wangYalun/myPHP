1b40087166bbb32de1a04806cea09c5ea15caf48
wangYalun <326402399@qq.com>
2017-02-22 18:26:25 +0800

    PHP 测试

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo "Hello World";
+
+$arr=array(
+"a"=>"a",
+"b"=>0
+);
+
+$arr2=array(
+"b"=>1
+);
+
+$a=array_merge($arr,$arr2);
+
+var_dump($arr);
+
+$str="sms";
+
+echo $str2="haha'{$arr[$arr['a']]}'";
